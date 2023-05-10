57e5d99cb4d14f46779ab5da97d4477da980d0cc
yuliia-stopkyna <styopkinayuliya@gmail.com>
2023-05-10 18:08:29 +0300

    Add models and admin panel for jobs, headers, applications

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,45 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Jobs(models.Model):
+
+    class TypeChoices(models.TextChoices):
+        FULL_TIME = "full-time"
+        PART_TIME = "part-time"
+
+    name = models.CharField(max_length=150)
+    type = models.CharField(max_length=10, choices=TypeChoices.choices)
+
+    class Meta:
+        db_table = "jobs"
+        verbose_name_plural = "jobs"
+
+    def __str__(self) -> str:
+        return f"Job ID: {self.id}, name: {self.name}"
+
+
+class JobHeaders(models.Model):
+    job = models.ForeignKey(Jobs, on_delete=models.CASCADE, related_name="headers")
+    rich_title_text = models.CharField(max_length=255)
+    rich_subtitle_text = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "job_headers"
+        verbose_name_plural = "job_headers"
+
+    def __str__(self) -> str:
+        return f"{self.rich_title_text}"
+
+
+class Applications(models.Model):
+    job = models.ForeignKey(Jobs, on_delete=models.CASCADE, related_name="applications")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="applications")
+
+    class Meta:
+        unique_together = ("job", "user")
+        db_table = "applications"
+        verbose_name_plural = "applications"
+
+    def __str__(self) -> str:
+        return f"Job ID: {self.job.id}, user ID: {self.user.id}"
