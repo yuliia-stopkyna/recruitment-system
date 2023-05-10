7bb411e54a02fb5285a63f1fe7dc06a3bd0e62cb
yuliia-stopkyna <styopkinayuliya@gmail.com>
2023-05-10 16:48:32 +0300

    Add serializer and endpoints for user create, manage, login

--- recruitment_system/settings.py
+++ recruitment_system/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -104,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
